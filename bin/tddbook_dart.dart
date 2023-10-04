03d52e36376e5c3181cdc32437ac2cdc9283ef6b
isanasan <yfcgpsebp@gmail.com>
2023-10-04 11:30:43 +0900

    run: dart create --pub . --force

---
+++ bin/tddbook_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tddbook_dart/tddbook_dart.dart' as tddbook_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tddbook_dart.calculate()}!');
+}
---
+++ test/tddbook_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tddbook_dart/tddbook_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
