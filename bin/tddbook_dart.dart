dfb109d0842551c98bdc7927edd4f5fe84c2d601
isanasan <yfcgpsebp@gmail.com>
2023-10-08 22:10:04 +0900

    delete: template

--- bin/tddbook_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tddbook_dart/tddbook_dart.dart' as tddbook_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tddbook_dart.calculate()}!');
-}
--- test/tddbook_dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tddbook_dart/tddbook_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
