353b85573c913633ace247e8fc5988452c58376a
isanasan <yfcgpsebp@gmail.com>
2023-10-22 22:29:25 +0900

    modify: p71

--- lib/money/dollar.dart
+++ lib/money/dollar.dart
@@ -2,9 +2,4 @@ import 'package:tddbook_dart/money/money.dart';
 
 class Dollar extends Money {
   Dollar(amount, currency) : super(amount, currency);
-
-  @override
-  Money times(int multiplier) {
-    return Dollar(amount * multiplier, super.currency);
-  }
 }
--- lib/money/franc.dart
+++ lib/money/franc.dart
@@ -2,9 +2,4 @@ import 'package:tddbook_dart/money/money.dart';
 
 class Franc extends Money {
   Franc(amount, currency) : super(amount, currency);
-
-  @override
-  Money times(int multiplier) {
-    return Franc(amount * multiplier, super.currency);
-  }
 }
