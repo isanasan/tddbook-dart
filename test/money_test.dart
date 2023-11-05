2b2425d2b72b3c924f1176eefc0058b79970b12c
isanasan <yfcgpsebp@gmail.com>
2023-11-05 21:39:23 +0900

    delete: learning test

--- test/money_test.dart
+++ test/money_test.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:tddbook_dart/money/money.dart';
 import 'package:tddbook_dart/money/expression.dart';
 import 'package:tddbook_dart/money/bank.dart';
@@ -58,20 +56,5 @@ void main() {
     bank.addRate("CHF", "USD", 2);
     Money result = bank.reduce(Money.franc(2), "USD");
     expect(result, Money.dollar(1));
-  });
-
-  test('array equals', () {
-    var test = {
-      {
-        "from": "USD",
-        "to": "CHF",
-      }: 2,
-    };
-    expect(
-        test[{
-          "from": "USD",
-          "to": "CHF",
-        }],
-        2);
   });
 }
