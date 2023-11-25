6d2d9981a7647256f93000940b452b2d85288e4c
isanasan <yfcgpsebp@gmail.com>
2023-11-25 23:18:51 +0900

    modify: 為替レートをBankから取得する

--- lib/money/bank.dart
+++ lib/money/bank.dart
@@ -1,14 +1,26 @@
 import 'package:tddbook_dart/money/expression.dart';
 import 'package:tddbook_dart/money/money.dart';
+import 'package:tddbook_dart/money/pair.dart';
 
 class Bank {
+  final _rates = <Pair, int>{};
+
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
 
-  void addRate(String from, String to, int rate) {}
+  void addRate(String from, String to, int rate) {
+    _rates[Pair(from, to)] = rate;
+  }
 
   int rate(String from, String to) {
-    return (from == "CHF" && to == "USD") ? 2 : 1;
+    int? result = _rates[Pair(from, to)];
+
+    if (result == null) {
+      // 本来はNotFoundRateExceptionを実装すべきだが今回は割愛する
+      throw Exception('Not found rate');
+    }
+
+    return result;
   }
 }
