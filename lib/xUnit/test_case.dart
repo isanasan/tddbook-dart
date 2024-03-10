e6d86b0e4bc540f903618909b199b69064e31fd5
isanasan <yfcgpsebp@gmail.com>
2024-03-10 21:50:05 +0900

    refact: p148

--- lib/xUnit/test_case.dart
+++ lib/xUnit/test_case.dart
@@ -1,5 +1,13 @@
+import 'dart:mirrors';
+
 class TestCase {
   String name;
 
   TestCase(this.name);
+
+  void run() {
+    var mirrror = reflect(this);
+    var symbol = Symbol(name);
+    mirrror.invoke(symbol, <dynamic>[]);
+  }
 }
--- lib/xUnit/was_run.dart
+++ lib/xUnit/was_run.dart
@@ -1,17 +1,9 @@
-import 'dart:mirrors';
-
 import 'package:tddbook_dart/xUnit/test_case.dart';
 
 class WasRun extends TestCase {
   bool wasRun = false;
 
   WasRun(name) : super(name);
-
-  void run() {
-    var mirrror = reflect(this);
-    var symbol = Symbol(name);
-    mirrror.invoke(symbol, <dynamic>[]);
-  }
 
   void testMethod() {
     wasRun = true;
