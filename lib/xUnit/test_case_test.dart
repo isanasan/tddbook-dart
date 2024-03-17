7f7eb5f5f9c6f5141c280cc4305eaa8880c17b3f
isanasan <yfcgpsebp@gmail.com>
2024-03-17 23:37:42 +0900

    refact: p180

--- lib/xUnit/test_case_test.dart
+++ lib/xUnit/test_case_test.dart
@@ -4,31 +4,34 @@ import 'package:tddbook_dart/xUnit/test_suite.dart';
 import 'package:tddbook_dart/xUnit/was_run.dart';
 
 class TestCaseTest extends TestCase {
+  late TestResult result;
+
   TestCaseTest(String name) : super(name);
 
+  @override
+  void setUp() {
+    result = TestResult();
+  }
+
   void testTemplateMethod() {
-    TestResult result = TestResult();
     WasRun test = WasRun("testMethod");
     test.run(result);
     assert("setUp testMethod tearDown " == test.log);
   }
 
   void testResult() {
-    TestResult result = TestResult();
     WasRun test = WasRun("testMethod");
     test.run(result);
     assert("1 run, 0 failed" == result.summary());
   }
 
   void testFailedResult() {
-    TestResult result = TestResult();
     WasRun test = WasRun("testBrokenMethod");
     test.run(result);
     assert("1 run, 1 failed" == result.summary());
   }
 
   void testFailedResultFormatting() {
-    TestResult result = TestResult();
     result.testStarted();
     result.testFailed();
     assert("1 run, 1 failed" == result.summary());
@@ -38,7 +41,6 @@ class TestCaseTest extends TestCase {
     TestSuite suite = TestSuite();
     suite.add(WasRun("testMethod"));
     suite.add(WasRun("testBrokenMethod"));
-    TestResult result = TestResult();
     suite.run(result);
     assert("2 run, 1 failed" == result.summary());
   }
